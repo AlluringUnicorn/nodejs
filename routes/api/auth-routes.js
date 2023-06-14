4c968fb353ab4931924aa0d434b057b118c82346
Mariia Khapun <111394270+AlluringUnicorn@users.noreply.github.com>
2023-06-14 17:46:03 +0300

    fixed/routes

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -7,8 +7,14 @@ const authController = require('../../controllers/auth-controller');
 
 const {validateBody} = require('../../decorators/validateBody');
 
-router.post("/signup", validateBody(schemas.userRegisterSchema), authController.signup);
+const authenticate = require('../../middlewares/authenticate');
 
-router.post("/signin", validateBody(schemas.userLoginSchema), authController.signin);
+router.post("/register", validateBody(schemas.userRegisterSchema), authController.register);
+
+router.post("/login", validateBody(schemas.userLoginSchema), authController.login);
+
+router.get("/current", authenticate, authController.getCurrent);
+
+router.post("/logout", authenticate, authController.logout);
 
 module.exports = router;
